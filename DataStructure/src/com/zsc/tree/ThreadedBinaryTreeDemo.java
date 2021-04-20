e7ccf66843bae4c4e0da95e5404300588bdcfc1e
zsc <z1s1c1@163.com>
2021-04-20 20:35:36 +0800

    tree

--- DataStructure/src/com/zsc/tree/ThreadedBinaryTreeDemo.java
+++ DataStructure/src/com/zsc/tree/ThreadedBinaryTreeDemo.java
@@ -54,24 +54,22 @@ class ThreadedBinaryTree {
     public void threadedList() {
         //定义一个变量，存储当前遍历的结点，从root开始
         HeroNode node = root;
-        while(node != null) {
+        while (node != null) {
             //循环的找到leftType == 1的结点，第一个找到就是8结点
             //后面随着遍历而变化,因为当leftType==1时，说明该结点是按照线索化处理后的有效结点
-            while(node.getLeftType() == 0) {
+            while (node.getLeftType() == 0) {
                 node = node.getLeft();
             }
-
             //打印当前这个结点
             System.out.println(node);
             //如果当前结点的右指针指向的是后继结点,就一直输出
-            while(node.getRightType() == 1) {
+            while (node.getRightType() == 1) {
                 //获取到当前结点的后继结点
                 node = node.getRight();
                 System.out.println(node);
             }
             //替换这个遍历的结点
             node = node.getRight();
-
         }
     }
     //编写对二叉树进行中序线索化的方法
